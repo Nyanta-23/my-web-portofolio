2dd7d9b44253bc6ae9d18d6ca4e62cf415c7701c
Nyanta-23 <muhamadilhan02404@gmail.com>
2024-08-08 12:15:36 +0700

    fix: basename for getting real path

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://nyanta-23.github.io/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
