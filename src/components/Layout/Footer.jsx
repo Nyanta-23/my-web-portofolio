a819eb06c62454e15b4023d519a673e309f9e0c5
Nyanta-23 <muhamadilhan02404@gmail.com>
2024-08-08 15:52:50 +0700

    fix: resolving footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className={`bg-custom-black h-20 text-custom-white z-10 relative`}>
-      <p className="text-center leading-[5rem]">
+    <footer className={`bg-custom-black text-custom-white z-10 relative `}>
+      <p className="text-center px-2 sm:leading-[5rem] py-10 sm:py-0">
         &#169; 2024, Created by <a href="#">Muhamad Ihan Revaliana Firmansyah</a>
       </p>
     </footer>
